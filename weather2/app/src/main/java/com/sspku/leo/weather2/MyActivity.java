84cbb5860dd9e332fa61455e50b05a28cc4eaa8a
leolee <814872839@qq.com>
2018-10-06 00:06:00 +0800

    First HomeWork

---
+++ weather2/app/src/main/java/com/sspku/leo/weather2/MyActivity.java
@@ -0,0 +1,13 @@
+package com.sspku.leo.weather2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_layout);
+    }
+}
